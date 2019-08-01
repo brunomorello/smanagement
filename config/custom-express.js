7180644a7e095dbbb0997d441102e35cbb327044
Bruno Moreno <brunomorello7@gmail.com>
2019-07-31 23:24:52 -0300

    added dao path to consign

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,11 +1,17 @@
 let express = require('express');
 let consign = require('consign');
+let bodyParser = require('body-parser');
 
 module.exports = function () {
+    
     let app = express();
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
 
     consign()
         .include('routes')
+        .then('dao')
         .into(app);
 
     return app;
