e6a973a5d68ac5268f94ade53ed2a968542fb071
Bruno Moreno <brunomorello7@gmail.com>
2019-08-12 23:13:22 -0300

    Updated host, port and database

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,6 +1,6 @@
-let express = require('express');
-let consign = require('consign');
-let bodyParser = require('body-parser');
+let express = require("express");
+let consign = require("consign");
+let bodyParser = require("body-parser");
 
 module.exports = () => {
     
