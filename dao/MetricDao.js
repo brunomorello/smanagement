d108fd0e409ec7cc7caaed3b38e683d2ac945c48
Bruno Moreno <brunomorello7@gmail.com>
2019-09-11 22:31:51 -0300

    Implemented PUT and DELETE verbs

--- dao/MetricDao.js
+++ dao/MetricDao.js
@@ -19,6 +19,14 @@ class MetricDao {
         this._connection.query('INSERT INTO metric SET ?', metric, callback);
     }
 
+    update(metric, callback) {
+        this._connection.query('UPDATE metric SET target = ? where id = ?', [metric.target, metric.id], callback);
+    }
+
+    inactive(metric, callback) {
+        this._connection.query('UPDATE metric SET status = ? where id = ?', [metric.status, metric.id], callback);
+    }
+
     selectAll(callback) {
         this._connection.query('SELECT * FROM metric', callback);
     }
--- routes/metrics.js
+++ routes/metrics.js
@@ -1,10 +1,56 @@
 const { check, validationResult } = require('express-validator');
+const MetricDao = require('../dao/MetricDao');
 
-module.exports = (app) => {
+module.exports = (app) => 
+{
     app.get('/metrics', (req, res) => {
        
         console.log('requisition for metrics received');
         res.send('ok');
+    });
+
+    app.delete('/metrics/metric/:id', (req, res) => {
+
+        let metric = req.body;
+        metric.id = req.params.id;
+        metric.status = 'inactive';
+
+        MetricDao.inactive(metric, (error, result) => {
+
+            if(error) {
+                res.status(500)
+                    .send(error);
+                throw error;
+            }
+            
+            console.log(`Executed: ${JSON.stringify(result)}`);
+
+            res.status(204);
+            res.json(metric);            
+
+        })
+
+    });
+
+    app.put('/metrics/metric/:id', (req, res) => {
+
+        let metric = req.body;
+        metric.id = req.params.id;
+
+        MetricDao.update(metric, (error, result) => {
+
+            if (error) {
+                res.status(500).send(error);
+                throw error;
+            }
+
+            console.log(`Executed: ${JSON.stringify(result)}`);
+
+            res.status(200);
+            res.json(metric);
+
+        });
+
     });
 
     app.post('/metrics/metric', [
@@ -29,8 +75,6 @@ module.exports = (app) => {
         metric.id = uuidv4();
         metric.status = 'active';
         metric.creation_date = new Date();
-
-        const MetricDao = require('../dao/MetricDao');
 
         MetricDao.insert(metric, (error, result) => {
             
