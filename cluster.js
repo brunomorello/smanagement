33b747f3278cec2664b45eac1c33a666a1bf33b9
Bruno Moreno <brunomorello7@gmail.com>
2019-10-02 21:56:23 -0300

    implemented cluster for this application

---
+++ cluster.js
@@ -0,0 +1,23 @@
+let cluster = require('cluster');
+let os = require('os');
+
+let cpus = os.cpus();
+
+if(cluster.isMaster) {
+
+    cpus.forEach(() => {
+        cluster.fork();
+    });
+
+    cluster.on('listening', worker => {
+        console.log(`cluster using PID ${worker.process.pid}`);
+    });
+
+    cluster.on('exit', worker => {
+        console.log(`clusted ${worker.process.pid} disconnected`);
+        cluster.fork();        
+    })
+
+} else {
+    require('./index.js');
+}
