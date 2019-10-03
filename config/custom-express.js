1cee7a455e51f9d395b68975b378b6198632f79c
Bruno Moreno <brunomorello7@gmail.com>
2019-10-02 21:27:41 -0300

    implemented morgan middleware logs to store it on fs

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,13 +1,19 @@
 let express = require("express");
 let consign = require("consign");
 let bodyParser = require("body-parser");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = () => {
     
     let app = express();
 
+    let accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/combined.log'), {flags: 'a' });
+
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
+    app.use(morgan('combined', { stream: accessLogStream }));
 
     consign()
         .include('routes')
