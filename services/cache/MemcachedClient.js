0829961b0bfc89262b0f012e0845141d0c969f77
Bruno Moreno <brunomorello7@gmail.com>
2019-10-11 00:06:38 -0300

    changed memcached hostname

--- services/cache/MemcachedClient.js
+++ services/cache/MemcachedClient.js
@@ -3,7 +3,7 @@ class Memcached {
     constructor () {
 
         let _memcached = require('memcached');
-        this._client = new _memcached('localhost:11211', {
+        this._client = new _memcached('smanagement_api_memcached:11211', {
             retries: 10,
             retry: 10000,
             remove: true
