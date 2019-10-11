8b3896ffd809727a104b88aa44aba11dfd43876b
Bruno Moreno <brunomorello7@gmail.com>
2019-10-11 00:05:58 -0300

    changed db hostname

--- dao/connectionFactory.js
+++ dao/connectionFactory.js
@@ -2,12 +2,20 @@ let mysql = require('mysql');
 
 exports.createDBConnection = () => {
 
+    const {
+        DATABASE_USERNAME,
+        DATABASE_PWD,
+        DATABASE_HOSTNAME,
+        DATABASE_PORT,
+        DATABASE_DB
+    } = process.env;
+
     return mysql.createConnection({
-        host: 'localhost',
-        port: '3306',
-        user: 'root',
-        password: 'dqm50vnc',
-        database: 'smanagement'
+        host: DATABASE_HOSTNAME,
+        port: DATABASE_PORT,
+        user: DATABASE_USERNAME,
+        password: DATABASE_PWD,
+        database: DATABASE_DB
     });
 
 }
