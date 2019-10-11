f013685d1cbb3e4d1b640b7c3ba5f99b8661d265
Bruno Moreno <brunomorello7@gmail.com>
2019-10-10 23:46:19 -0300

    Dockerized API

---
+++ build/mariadb/sql/smanagment-schema.sql
@@ -0,0 +1,10 @@
+use `smanagement`;
+
+CREATE TABLE IF NOT EXISTS `metric` (
+	`id` varchar(36) NOT NULL, 
+	`name` varchar(50) NOT NULL,
+	`target` int(5) NOT NULL,
+	`status` varchar(10) NOT NULL,
+	`creation_date` timestamp NOT NULL,
+	CONSTRAINT `metric_pk` PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
