47f087582e95fc920f993436cbf9719d74064dcb
Bruno Moreno <brunomorello7@gmail.com>
2019-10-21 23:33:27 -0300

    Developed Connection Factory for IBM Cloudant

--- dao/cloudantConnectionFactory.js
+++ dao/cloudantConnectionFactory.js
@@ -1,47 +1,24 @@
 const Cloudant = require('@cloudant/cloudant');
 
-let cloudant = new Cloudant({
-    url: 'https://05cfc43d-1c86-4ae5-bbf9-a9ffb079f4ae-bluemix.cloudantnosqldb.appdomain.cloud',
-    plugins: {
-        iamauth: {
-            iamApiKey: 'iG-1m5wiHQfQz-OAp91bOFCzmiDJ3v2bt9g1MMtWkI6O'
+exports.createDBConnection = () => {
+
+    // get Env vars
+    const {
+        CLOUDANT_URL,
+        CLOUDANT_IAMKEY,
+        CLOUDANT_DB_INSTANCE
+    } = process.env;
+
+
+    let cloudant = new Cloudant({
+        url: 'https://05cfc43d-1c86-4ae5-bbf9-a9ffb079f4ae-bluemix.cloudantnosqldb.appdomain.cloud',
+        plugins: {
+            iamauth: {
+                iamApiKey: 'iG-1m5wiHQfQz-OAp91bOFCzmiDJ3v2bt9g1MMtWkI6O'
+            }
         }
-    }
-});
+    });
 
-let db = cloudant.db.use('smanagement-api');
+   return cloudant.db.use('smanagement-api');
 
-let fakeMetric = {
-    "_id": "metric:ee399169-1202-4382-b9a2-2563e1d06a86",
-    "name": "test",
-    "target": "90"
 };
-
-db.insert(fakeMetric, (err, data) => {
-
-    console.log(`Error ${err}`);
-    console.log(`Data ${JSON.stringify(data)}`);
-
-});
-
-db.get(fakeMetric._id, (err, data) => {
-
-    console.log(`Error ${err}`);
-    console.log(`Data ${JSON.stringify(data)}`);    
-
-    fakeMetric = data;
-
-});
-
-fakeMetric.name = "test 2";
-
-db.insert(fakeMetric, (err,data) => {
-    console.log(`Error ${err}`);
-    console.log(`Data ${JSON.stringify(data)}`);    
-
-})
-
-db.get(fakeMetric._id, (err, data) => {
-    console.log(`Error ${err}`);
-    console.log(`Data ${JSON.stringify(data)}`);    
-});
