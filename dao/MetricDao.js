93de32c9405eef245587b5b02513f4e9afb6373b
Bruno Moreno <brunomorello7@gmail.com>
2019-10-21 23:37:59 -0300

    Refactor to use Cloudant instead of MYSQL

--- dao/MetricDao.js
+++ dao/MetricDao.js
@@ -2,38 +2,44 @@ class MetricDao {
  
     constructor() {
 
-        let mysqlConnection = require('./connectionFactory');
+        // mysql decommisioned - comments remained only for studies purposes
+        //let mysqlConnection = require('./connectionFactory');
+        let cloudantFactory = require('./cloudantConnectionFactory');
 
-        this._connection = mysqlConnection.createDBConnection();
+        this._connection = cloudantFactory.createDBConnection();
         
-        this._connection.connect((err) => {
+        // this._connection.connect((err) => {
            
-            if (err) throw err;
+        //     if (err) throw err;
 
-            console.log(`connected!`);            
+        //     console.log(`connected!`);            
 
-        });
+        // });
     }
 
     insert(metric, callback) {
-        this._connection.query('INSERT INTO metric SET ?', metric, callback);
+        //this._connection.query('INSERT INTO metric SET ?', metric, callback);
+        this._connection.insert(metric, callback);
     }
 
-    update(metric, callback) {
-        this._connection.query('UPDATE metric SET target = ? where id = ?', [metric.target, metric.id], callback);
-    }
 
-    inactive(metric, callback) {
-        this._connection.query('UPDATE metric SET status = ? where id = ?', [metric.status, metric.id], callback);
+    selectAll(tableName) {
+        //this._connection.query('SELECT * FROM metric', callback);
+        return this._connection.partitionedList(tableName, { include_docs: true });
     }
-
-    selectAll(callback) {
-        this._connection.query('SELECT * FROM metric', callback);
-    }
-
+    
     get(id, callback) {
-        this._connection.query('SELECT * FROM metric WHERE id = ?', id, callback);
+        //this._connection.query('SELECT * FROM metric WHERE id = ?', id, callback);
+        this._connection.get(id, callback);
     }
+
+    // update(metric, callback) {
+    //     this._connection.query('UPDATE metric SET target = ? where id = ?', [metric.target, metric.id], callback);
+    // }
+
+    // inactive(metric, callback) {
+    //     this._connection.query('UPDATE metric SET status = ? where id = ?', [metric.status, metric.id], callback);
+    // }
 
 }
 
